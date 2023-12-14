6271eb5ace7e80d3453b92883cd641892457fb85
shad anjum <92822708+shad-pythondev123@users.noreply.github.com>
2023-12-14 16:27:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 var isDate = function (input) {
   //   write your code here
+	if (input instanceof Date) {
+        // If the input is already a Date object, return true
+        return true;
+    } else if (typeof input === 'string') {
+        // If the input is a string, attempt to parse it as a date
+        const parsedDate = new Date(input);
+        // Check if the parsed date is a valid date
+        return !isNaN(parsedDate.getTime());
+    } else {
+        // If the input is neither a Date object nor a string, return false
+        return false;
+    }
 };
 
 // Do not change the code below.
-const input = prompt("Enter Date.");
-alert(isDate(input));
+// const input = prompt("Enter Date.");
+// alert(isDate(input));
